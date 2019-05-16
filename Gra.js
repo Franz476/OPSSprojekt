1d7dcb47aed2e28bb51f998fc3a973156e4ced4f
Franz476 <48163310+Franz476@users.noreply.github.com>
2019-05-16 20:48:00 +0200

    Update Gra.js

--- Gra.js
+++ Gra.js
@@ -1,1 +1,12 @@
-var canvas = document.GetElementById("
+var canvas = document.GetElementById('Pong');
+var ctx = canvas.getContext('2d');
+var gra = {}; //trzymać będzie wszystkie informacje o stanie gry.
+
+//konstruktor pilki
+
+function ball() 
+{
+  this.x = 0;
+  this.y = 0;
+  this.promien = 5;
+  this.kolor = (
